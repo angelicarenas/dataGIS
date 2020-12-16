a0dfc71d0eb3f565eb2de88202c9a46b7873174a
angelicarenas <47420537+angelicarenas@users.noreply.github.com>
2020-12-15 20:22:36 -0500

    Update code_RGBIF.R

--- code_RGBIF.R
+++ code_RGBIF.R
@@ -5,9 +5,10 @@ loadandinstall ("rgbif")
 loadandinstall ("maptools")
 loadandinstall ("rgeos")
 
-head(name_suggest(q='Platyrrhinus chocoensis'))
-ek<-occ_search(taxonKey= 2487408,return="all",limit=200000,hasCoordinate=TRUE)
-write.csv(c(ek$data,ek$meta),file="C:/Users/fam/Downloads/Mapas_datosGBIF/datos_GBIF/Vireo philadelphicus.csv")
+sp<- gbif("Eretmochelys","imbricata", download = T, geo = T, sp=F)
+head(name_suggest(q='Eretmochelys imbricata'))
+ek<-occ_search(taxonKey= 2442207,return="all",limit=200000,hasCoordinate=TRUE)
+write.csv(c(ek$data,ek$meta),file="C:/Users/fam/Downloads/Mapas_datosGBIF/datos_GBIF/Eretmochelys imbricata.csv")
 datos<-cbind(ek$data,ek$meta)
 dups <- duplicated(datos[,3:4])
 datos2<-cbind(dups,datos)
@@ -23,14 +24,14 @@ plot(wrld_simpl)
 plot(datos51[,3])
 mapa<-plot(wrld_simpl,xlim=c(-150,150),ylim=c(-90,90),axes=TRUE,col="green")
 puntos<-points(datos51$decimalLongitude,datos5$decimalLatitude, col="red", pch=20, cex=0.9)
-title(main= "Ruta migratoria mundial de Platyrrhinus chocoensis", cex.main = 1, font.main = 1)
+title(main= "Ruta migratoria mundial de Eretmochelys imbricata", cex.main = 1, font.main = 1)
 
 Colombia<-wrld_simpl[wrld_simpl$NAME=="Colombia", ]
 proj4string(datos51)<- proj4string(Colombia)
 plot(Colombia, border="grey", axes=TRUE,col="green")
 Colab<-datos51[Colombia, ]
 plot(Colab, add=T, col="blue", pch=20, cex=0.9)
-title(main= "Ruta migratoria en Colombia Platyrrhinus chocoensis", cex.main = 1, font.main = 1)
+title(main= "Ruta migratoria en Colombia Eretmochelys imbricata", cex.main = 1, font.main = 1)
 
 ###FUENTE DE LOS DATOS - Modificado de: ############
 ## Chamberlain, S. (2017). rgbif: Interface to the Global Biodiversity Information Facility API. R package version 0.9.8. https://CRAN.R-project.org/package=rgbif
